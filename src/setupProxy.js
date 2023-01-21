0330ac88ef4224c44359aefc7666eb4ce66cf7d3
Gaofan Lin <2583643036@qq.com>
2023-01-21 23:23:35 +0800

    更改跨域解决方式

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,8 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware('/api',{
+    '/api',
+    createProxyMiddleware({
       target: 'http://baobab.kaiyanapp.com/api',
       pathRewrite:{'^/api': ''} ,
       changeOrigin: true
